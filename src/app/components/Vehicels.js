4d5a7a6656c2672c7a4471b519b0082fb71725b9
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-10-07 00:00:14 +0630

    for vehicels

--- src/app/components/Vehicels.js
+++ src/app/components/Vehicels.js
@@ -5,7 +5,7 @@ const Vehicels = () => {
 
     return (
 
-        <div className='mb-4  vehicle'>
+        <div className='mb-4 vehicle'>
             <div className='mx-auto  w-[80%]'>
                 <div className='py-10 pb-0 text-[20px] text-[#292524] font-semibold tracking-wider opacity-90'>
                     3 Available Products
@@ -13,6 +13,10 @@ const Vehicels = () => {
                 <div className='product-border mt-4 bg-[#bfbfbf]'></div>
 
                 <div class="py-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5  ">
+                    <VehiclesCard />
+                    <VehiclesCard />
+                    <VehiclesCard />
+                    <VehiclesCard />
                     <VehiclesCard />
                     <VehiclesCard />
                     <VehiclesCard />

5bcca043041c29c628c49b111869321b54132e6f
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-10-20 23:42:54 +0630

    for searchbox

--- src/app/components/SearchBox.js
+++ src/app/components/SearchBox.js
@@ -1,13 +1,54 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import { Data } from "../search/components/Data";
 
-const 
- = () => {
+function SearchBox() {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = Data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
   return (
-    <div>
-      SearchBox
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder="Enter..."
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataItem" href={value.link} target="_blank">
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default 
-
+export default SearchBox;
---
+++ src/app/search/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBox from '../components/SearchBox'
+
+const page = () => {
+  return (
+    <div className='App'>
+        <SearchBox />
+    </div>
+  )
+}
+
+export default page
